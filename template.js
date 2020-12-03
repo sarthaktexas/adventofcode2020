65a60b651ce21ad0e85e4f8e34db74a1b63d227b
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-12-03 06:37:10 +0000

    ✨ template now contains counter and forEach

--- template.js
+++ template.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 
 let filename = 'filename.txt'
+let i = 0;
 
 fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
     const lineArray = data.split('\n');
+    lineArray.forEach(line => {
+        console.log(line);
+        i++
+    });
 });
