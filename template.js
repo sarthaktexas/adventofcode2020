ec21f6a941cf4caceea9cb872a29e0fa11f5a22a
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-12-07 05:24:51 +0000

    🚑 change lineArray to let so asignmnt can be made

--- template.js
+++ template.js
@@ -6,7 +6,7 @@ let i = 0;
 
 fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
-    const lineArray = data.split('\n');
+    let lineArray = data.split('\n');
     lineArray.forEach(line => {
         console.log(line);
         i++
